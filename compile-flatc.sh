d9ba06e7689288aff394b4da6fbdadb0bf076c5e
zealchen <283559115@qq.com>
2024-05-06 07:55:54 -0700

    modify

--- compile-flatc.sh
+++ compile-flatc.sh
@@ -1,11 +1,11 @@
 wget 'https://github.com/google/flatbuffers/archive/refs/tags/v24.3.25.tar.gz'
 
 tar xzf v24.3.25.tar.gz
-pushd flatbuffers-24.3.25
+cd flatbuffers-24.3.25
 cmake -G "Unix Makefiles"
 make
 make install
 ldconfig
 flatc --version
-popd
+cd -
 rm -rf flatbuffers-24.3.25
